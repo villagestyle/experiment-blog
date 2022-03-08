42437348c09394c3ec9acf5c6da2aae66cfe6240
suhang_xie <a1164995332@163.com>
2022-03-08 10:29:18 +0800

    🔧工具: 全局使用 Scss Mixin

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,19 @@ export default defineConfig({
       src: resolve("src"),
       assets: resolve("src/assets")
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        /*
+          引入var.scss全局预定义变量，
+          如果引入多个文件，
+          可以使用
+          '@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+          这种格式
+         */
+        additionalData: '@import "src/styles/var.scss";'
+      }
+    }
   }
 });
