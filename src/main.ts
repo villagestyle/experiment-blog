ba8d76d4424ee513f25467103ba77053e21979a7
suhang_xie <a1164995332@163.com>
2022-03-08 10:58:42 +0800

    🔧工具: 删除 Router 没用的配置

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router, { setupRouter } from "./router"
+import router from "./router"
 import store from "./store"
 
 const app = createApp(App)
 
-setupRouter(app)
+app.use(router);
 app.use(store)
 
 router.isReady().then(() => {
