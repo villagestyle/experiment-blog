dbc3667b21bb1967e3648cbd34dc142c36267ace
suhang_xie <a1164995332@163.com>
2022-03-10 17:02:00 +0800

    🚧提交: 文章详情完善

---
+++ src/views/article/components/Comment.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- 评论组件 -->
+  <div class="conment-box">
+    <div class="portrait"></div>
+
+    <div class="conment">
+      <div class="user-box">
+        <span>名字</span>
+        <span>5小时前</span>
+      </div>
+
+      <div class="content">
+        全部评论全部评论全部评论全部评论全部评论全部评论全部评论全部评论全部评论全部评论全部评论
+      </div>
+
+      <div class="action-box">
+        <div class="icon--like"><span>点赞</span></div>
+        <div class="icon--reply"><span>回复</span></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.conment-box {
+  display: flex;
+  .portrait {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    flex-shrink: 0;
+    background-color: #f4f5f5;
+  }
+  .conment {
+    width: 100%;
+    margin-left: 20px;
+    .user-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 22px;
+      > span:nth-child(1) {
+        font-size: 15px;
+        color: $c-normal;
+      }
+      > span:nth-child(2) {
+        font-size: 14px;
+        color: $c-tips;
+      }
+    }
+    .content {
+      font-weight: 400;
+      font-size: 14px;
+      color: $c-normal;
+      margin-top: 8px;
+      line-height: 1.6;
+      @include line-2;
+      -webkit-line-clamp: 3;
+    }
+    .action-box {
+      display: flex;
+      margin-top: 8px;
+      color: $c-tips;
+      > div {
+        font-size: 14px;
+        > span {
+          margin-left: 5px;
+        }
+      }
+      .icon--reply {
+        &::before {
+          position: relative;
+          top: 1px;
+        }
+      }
+      > div + div {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,9 +1,285 @@
 <template>
-  <div>1</div>
+  <div class="container">
+    <main class="main">
+      <section class="article-container">
+        <div class="article-main">
+          <div class="title">题目题目</div>
+
+          <div class="describe">
+            <span>2月 24, 2022</span>
+            <span>阅读<span>35</span></span>
+            <span>字数<span>1911</span></span>
+            <span>评论<span>0</span></span>
+            <span>喜欢<span>1</span></span>
+          </div>
+
+          <div class="content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry’s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry’s standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry’s standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry’s standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum
+            passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry’s standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged. It was popularised in the 1960s with the
+            release of Letraset sheets containing Lorem Ipsum passages, and more
+            recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text
+            of the printing and typesetting industry. Lorem Ipsum has been the
+            industry’s standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </div>
+        </div>
+      </section>
+
+      <section class="comment-container">
+        <div class="conment-main">
+          <div class="header">评论</div>
+
+          <div class="input-box">
+            <div class="portrait"></div>
+
+            <el-input
+              class="input"
+              type="textarea"
+              :rows="3"
+              resize="none"
+              placeholder="输入评论（Enter换行）"
+            />
+          </div>
+
+          <div class="header">全部评论 1</div>
+
+          <div class="content-box">
+            <div class="item" v-for="item in data" :key="item.id">
+              <Comment />
+
+              <div class="sub-item" v-if="item.sub">
+                <Comment />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="recommend-container">
+        <div class="recommend-main">
+          <div class="header">相关文章</div>
+
+          <ul class="content-box">
+            <li class="item" v-for="item in data" :key="item.id">
+              <div>
+                <span class="title">题目题目2</span>
+
+                <div class="content">
+                  王大司马家里很有钱。一天，他忽然梦见一个人对他说：“你欠我四十千钱，现在该还我了。”然后径直进了屋内。
+                  他惊醒，妻子正巧生了个男孩。看来这孩子是来算上辈子的帐王大司马家里很有钱。一天，他忽然梦见一个人对他
+                  说：“你欠我四十千钱，现在该还我了。”然后径直进了屋内。他惊醒，妻子正巧生了个男孩。看来这孩子是来算上辈子的帐
+                </div>
+              </div>
+
+              <div class="image">
+                <img
+                  src="https://w.laborers.cn/wp-content/themes/Diaspora/timthumb/timthumb.php?src=https://w.laborers.cn/wp-content/uploads/2022/02/wallhaven-674018-1.jpeg"
+                  alt=""
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from "vue";
+import Comment from "./components/Comment.vue";
+
+const data = reactive([{ id: 1, sub: true }, { id: 2 }]);
 </script>
 
 <style lang="scss" scoped>
+.container {
+  background-color: #f4f5f5;
+  .article-container {
+    background-color: #fff;
+    .article-main {
+      width: 700px;
+      margin: 0 auto;
+      padding: 50px 0;
+      .title {
+        color: $c-main;
+        font-weight: 600;
+        font-size: 28px;
+      }
+      .describe {
+        font-size: 13px;
+        color: $c-normal;
+        margin-top: 20px;
+        > span {
+          > span {
+            margin-left: 6px;
+          }
+        }
+        > span + span {
+          margin-left: 30px;
+        }
+        &::after {
+          content: "";
+          height: 1px;
+          width: 100px;
+          display: block;
+          background: $bg-c;
+          margin: 0 auto;
+          margin-top: 30px;
+        }
+      }
+      .content {
+        margin-top: 40px;
+        font-size: 14px;
+        line-height: 2;
+      }
+    }
+  }
+  .comment-container {
+    padding: 50px 0;
+    .conment-main {
+      width: 700px;
+      margin: 0 auto;
+      padding: 30px;
+      background-color: #fff;
+      .header {
+        font-size: 18px;
+        font-weight: 600;
+        margin-bottom: 20px;
+      }
+      .portrait {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        flex-shrink: 0;
+        background-color: #f4f5f5;
+      }
+      .input-box {
+        display: flex;
+        margin-bottom: 50px;
+        .input {
+          margin-left: 20px;
+          & :deep(.el-textarea__inner) {
+            border: none;
+            background-color: #f2f3f5;
+            &::-webkit-input-placeholder {
+              color: #8a919f;
+            }
+          }
+        }
+      }
+      .content-box {
+        .item {
+          .sub-item {
+            width: calc(100% - 40px);
+            margin-top: 20px;
+            margin-left: auto;
+            padding: 20px;
+            border-radius: 4px;
+            background: rgba(247, 248, 250, 0.7);
+          }
+        }
+        .item + .item {
+          margin-top: 30px;
+        }
+      }
+    }
+  }
+  .recommend-container {
+    padding-bottom: 50px;
+    .recommend-main {
+      width: 700px;
+      margin: 0 auto;
+      padding: 30px;
+      background-color: #fff;
+      .header {
+        font-size: 18px;
+        font-weight: 600;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #e8e8e8;
+      }
+      .content-box {
+        .item {
+          display: flex;
+          padding: 25px 0;
+          border-bottom: 1px solid #e8e8e8;
+          > div:nth-child(1) {
+            width: calc(100% - 200px);
+            height: 100px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            .title {
+              font-size: 16px;
+              font-weight: 600;
+            }
+            .content {
+              font-size: 14px;
+              color: $c-normal;
+              line-height: 1.6;
+              @include line-2;
+              -webkit-line-clamp: 3;
+            }
+          }
+          > div:nth-child(2) {
+            width: 200px;
+            height: 100px;
+            padding-left: 20px;
+            > img {
+              object-fit: cover;
+              width: 100%;
+              height: 100%;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
