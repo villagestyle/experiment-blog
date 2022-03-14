2e7816f1c4329cf76a437bab4d3b72888ed97a9d
suhang_xie <a1164995332@163.com>
2022-03-14 17:33:33 +0800

    🚧提交: 登录功能

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,8 +1,18 @@
+import { setToken, setUserInfo } from "src/utils/auth";
+
 const state = {
+  token: '',
   info: {}
 };
 
 const mutations = {
+  SET_TOKEN: (state: any, value: any) => {
+    state.token = value
+  },
+
+  SET_USER_INFO: (state: any, value: any) => {
+    state.info = value
+  }
 };
 
 const actions = {
@@ -11,6 +21,12 @@ const actions = {
     userInfo.username = userInfo.username.trim()
     return new Promise((resolve, reject) => {
       if (userInfo.username === 'admin' && userInfo.password === '12345567') {
+        setToken('0123456789')
+        setUserInfo(userInfo)
+
+        commit('SET_TOKEN', '0123456789')
+        commit('SET_USER_INFO', userInfo)
+
         resolve(true)
       } else {
         reject()
