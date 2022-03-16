6ffad81ac8aa260e764f38f1ef3b3ad67d056814
suhang_xie <a1164995332@163.com>
2022-03-16 13:37:51 +0800

    🚧提交: layout 布局页面

---
+++ src/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-wrapper">
+    <div class="main-container">
+      <!-- 导航条 -->
+      <Nav />
+
+      <!-- 主要内容 -->
+      <section class="app-main">
+        <transition name="fade" mode="out-in">
+          <router-view />
+        </transition>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
